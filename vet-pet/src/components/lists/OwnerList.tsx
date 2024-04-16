2f21957a6cbb4670b853edb0e74779328e8dea4f
Tobias Johansson <tobiasp91@hotmail.com>
2024-04-16 14:39:29 +0200

    added double check when deleteing a owner. owner is deleted permanently from db

--- vet-pet/src/components/lists/OwnerList.tsx
+++ vet-pet/src/components/lists/OwnerList.tsx
@@ -3,8 +3,18 @@ import { instance } from "../../services/AxiosInstance";
 import {
   Box,
   Button,
+  Flex,
   FormLabel,
   Grid,
+  Icon,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
   Table,
   TableContainer,
   TagLabel,
@@ -15,6 +25,7 @@ import {
   Th,
   Thead,
   Tr,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { deleteOwner } from "../../services/api/crud-owner";
 
@@ -35,6 +46,9 @@ interface Owner {
 
 const OwnerList = () => {
   const [ownerList, setOwners] = useState<Owner[]>([]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [deletingOwnerId, setDeletingOwnerId] = useState<number | null>(null);
+
   const [expandedOwner, setExpandedOwner] = useState(null);
   const handleSetOwners = useCallback((data: any) => {
     setOwners(data);
@@ -58,6 +72,7 @@ const OwnerList = () => {
   };
 
   const handleDelete = async (id: number) => {
+    onOpen();
     try {
       // Optimistically remove the owner from the list
       const updatedOwners = ownerList.filter((owner) => owner.ownerId !== id);
@@ -75,76 +90,121 @@ const OwnerList = () => {
   };
 
   return (
-    <TableContainer>
-      <Table variant="striped" colorScheme="teal">
-        {!expandedOwner && (
-          <Thead>
-            <Tr>
-              <Th>Owner Name</Th>
-              <Th>Pet Name</Th>
-            </Tr>
-          </Thead>
-        )}
-        <Tbody>
-          {ownerList.map((owner) => (
-            <React.Fragment key={owner.ownerId}>
-              {!expandedOwner && (
-                <Tr
-                  onClick={() => toggleExpandedOwner(owner.ownerId)}
-                  style={{ cursor: "pointer" }}
-                >
-                  <Td>{owner.name}</Td>
-                  <Td>{owner.pets ? owner.pets.name : "No pet"}</Td>
-                </Tr>
-              )}
-              {expandedOwner === owner.ownerId && (
-                <Td onClick={() => toggleExpandedOwner(owner.ownerId)}>
-                  <Grid templateColumns="1fr 1fr" gap={4}>
-                    <Box p={4}>
-                      <Text>
-                        <strong>Owner Name:</strong> {owner.name}
-                      </Text>
-                      <Text mt={2}>
-                        <strong>Owner ID:</strong> {owner.ownerId}
-                      </Text>
-                      <Text mt={2}>
-                        <strong>Address:</strong> {owner.address.street}
-                      </Text>
-                      <Text mt={2}>
-                        <strong>Phone:</strong> {owner.address.phone}
-                      </Text>
-                    </Box>
-                    <Box p={4}>
-                      <Text>
-                        <strong>Pet Name:</strong> {owner.pets.name}
-                      </Text>
-                      <Text mt={2}>
-                        <strong>Age:</strong> {owner.pets.age}
-                      </Text>
-                      <Text mt={2}>
-                        <strong>Gender:</strong> {owner.pets.gender}
-                      </Text>
-                      <Text mt={2}>
-                        <FormLabel>
-                          <strong>Comment:</strong>
-                        </FormLabel>
-                        <Textarea bg="white">{owner.pets.comment}</Textarea>
-                      </Text>
-                    </Box>
-                  </Grid>
-                  <Button
-                    bg="red.400"
-                    onClick={() => handleDelete(owner.ownerId)}
+    <>
+      <TableContainer>
+        <Table variant="striped" colorScheme="teal">
+          {!expandedOwner && (
+            <Thead>
+              <Tr>
+                <Th>Owner Name</Th>
+                <Th>Pet Name</Th>
+              </Tr>
+            </Thead>
+          )}
+          <Tbody>
+            {ownerList.map((owner) => (
+              <React.Fragment key={owner.ownerId}>
+                {!expandedOwner && (
+                  <Tr
+                    onClick={() => toggleExpandedOwner(owner.ownerId)}
+                    style={{ cursor: "pointer" }}
                   >
-                    Delete
-                  </Button>
-                </Td>
-              )}
-            </React.Fragment>
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
+                    <Td>{owner.name}</Td>
+                    <Td>{owner.pets ? owner.pets.name : "No pet"}</Td>
+                  </Tr>
+                )}
+                {expandedOwner === owner.ownerId && (
+                  <Tr onClick={() => toggleExpandedOwner(owner.ownerId)}>
+                    <Td colSpan={2}>
+                      <Grid templateColumns="1fr 1fr" gap={4}>
+                        <Box p={4}>
+                          <Text>
+                            <strong>Owner Name:</strong> {owner.name}
+                          </Text>
+                          <Text mt={2}>
+                            <strong>Owner ID:</strong> {owner.ownerId}
+                          </Text>
+                          <Text mt={2}>
+                            <strong>Address:</strong> {owner.address.street}
+                          </Text>
+                          <Text mt={2}>
+                            <strong>Phone:</strong> {owner.address.phone}
+                          </Text>
+                        </Box>
+                        <Box p={4}>
+                          <Text>
+                            <strong>Pet Name:</strong> {owner.pets.name}
+                          </Text>
+                          <Text mt={2}>
+                            <strong>Age:</strong> {owner.pets.age}
+                          </Text>
+                          <Text mt={2}>
+                            <strong>Gender:</strong> {owner.pets.gender}
+                          </Text>
+                          <Text mt={2}>
+                            <FormLabel>
+                              <strong>Comment:</strong>
+                            </FormLabel>
+                            <Textarea
+                              bg="white"
+                              value={owner.pets.comment}
+                              readOnly
+                            />
+                          </Text>
+                        </Box>
+                      </Grid>
+                      <Button
+                        bg="red.400"
+                        onClick={() => {
+                          setDeletingOwnerId(owner.ownerId);
+                          onOpen();
+                        }}
+                      >
+                        Delete
+                      </Button>
+                    </Td>
+                  </Tr>
+                )}
+              </React.Fragment>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <Modal size="md" onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Warning: Deleting Owner</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>Do you want to delete the owner? 🐾</Text>
+            <Text>
+              Are you sure you want to delete this owner? This action cannot be
+              undone and will permanently remove all associated data from the
+              database.
+            </Text>
+          </ModalBody>
+          <ModalFooter>
+            <Flex alignItems="center">
+              <Text>(555) 123-4567: call support</Text>
+            </Flex>
+            <Spacer />
+            <Button
+              onClick={() => {
+                if (deletingOwnerId !== null) {
+                  console.log("id", deletingOwnerId);
+                  handleDelete(deletingOwnerId);
+                  setDeletingOwnerId(null);
+                  onClose();
+                }
+              }}
+              colorScheme="red"
+            >
+              Delete
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
