1fa4182053f31f2303e0aea3fcd33b623b3ca4bc
Tobias Johansson <tobiasp91@hotmail.com>
2024-04-16 11:40:35 +0200

    refactoring the owner list to make it more user friendly

--- vet-pet/src/components/lists/OwnerList.tsx
+++ vet-pet/src/components/lists/OwnerList.tsx
@@ -2,12 +2,15 @@ import React, { useCallback, useEffect, useState } from "react";
 import { instance } from "../../services/AxiosInstance";
 import {
   Box,
+  FormLabel,
   Grid,
   Table,
   TableContainer,
+  TagLabel,
   Tbody,
   Td,
   Text,
+  Textarea,
   Th,
   Thead,
   Tr,
@@ -55,54 +58,62 @@ const OwnerList = () => {
   return (
     <TableContainer>
       <Table variant="striped" colorScheme="teal">
-        <Thead>
-          <Tr>
-            <Th>Owner Name</Th>
-            <Th>Pet Name</Th>
-          </Tr>
-        </Thead>
+        {!expandedOwner && (
+          <Thead>
+            <Tr>
+              <Th>Owner Name</Th>
+              <Th>Pet Name</Th>
+            </Tr>
+          </Thead>
+        )}
         <Tbody>
-          {ownerList.map((owner) => (
+          {ownerList.map((owner, index) => (
             <React.Fragment key={owner.ownerId}>
-              <Tr
-                onClick={() => toggleExpandedOwner(owner.ownerId)}
-                style={{ cursor: "pointer" }}
-              >
-                <Td>{owner.name}</Td>
-                <Td>{owner.pets ? owner.pets.name : "No pet"}</Td>
-              </Tr>
-              {expandedOwner === owner.ownerId && (
-                <Tr>
-                  <Td colSpan={4}>
-                    <Grid templateColumns="1fr 1fr" gap={4}>
-                      <Box p={4}>
-                        <Text>
-                          <strong>Owner ID:</strong> {owner.ownerId}
-                        </Text>
-                        <Text>
-                          <strong>Address:</strong> {owner.address.street}
-                        </Text>
-                        <Text>
-                          <strong>Phone:</strong> {owner.address.phone}
-                        </Text>
-                      </Box>
-                      <Box p={4}>
-                        <Text>
-                          <strong>Pet Name:</strong> {owner.pets.name}
-                        </Text>
-                        <Text>
-                          <strong>Age:</strong> {owner.pets.age}
-                        </Text>
-                        <Text>
-                          <strong>Gender:</strong> {owner.pets.gender}
-                        </Text>
-                        <Text>
-                          <strong>Comment:</strong> {owner.pets.comment}
-                        </Text>
-                      </Box>
-                    </Grid>
-                  </Td>
+              {!expandedOwner && (
+                <Tr
+                  onClick={() => toggleExpandedOwner(owner.ownerId)}
+                  style={{ cursor: "pointer" }}
+                >
+                  <Td>{owner.name}</Td>
+                  <Td>{owner.pets ? owner.pets.name : "No pet"}</Td>
                 </Tr>
+              )}
+              {expandedOwner === owner.ownerId && (
+                <Td onClick={() => toggleExpandedOwner(owner.ownerId)}>
+                  <Grid templateColumns="1fr 1fr" gap={4}>
+                    <Box p={4}>
+                      <Text>
+                        <strong>Owner Name:</strong> {owner.name}
+                      </Text>
+                      <Text mt={2}>
+                        <strong>Owner ID:</strong> {owner.ownerId}
+                      </Text>
+                      <Text mt={2}>
+                        <strong>Address:</strong> {owner.address.street}
+                      </Text>
+                      <Text mt={2}>
+                        <strong>Phone:</strong> {owner.address.phone}
+                      </Text>
+                    </Box>
+                    <Box p={4}>
+                      <Text>
+                        <strong>Pet Name:</strong> {owner.pets.name}
+                      </Text>
+                      <Text mt={2}>
+                        <strong>Age:</strong> {owner.pets.age}
+                      </Text>
+                      <Text mt={2}>
+                        <strong>Gender:</strong> {owner.pets.gender}
+                      </Text>
+                      <Text mt={2}>
+                        <FormLabel>
+                          <strong>Comment:</strong>
+                        </FormLabel>
+                        <Textarea bg="white">{owner.pets.comment}</Textarea>
+                      </Text>
+                    </Box>
+                  </Grid>
+                </Td>
               )}
             </React.Fragment>
           ))}
